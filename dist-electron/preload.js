5f957587d0766ffa34dbd60181fe9102fef3c9a6
Quanyu Qi <qi@mai.team>
2026-03-30 05:19:44 +0900

    Remove WIP and unused files

--- dist-electron/preload.js
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electron', {
-    openAudioFile: () => ipcRenderer.invoke('open-audio-file'),
-    saveFile: (options) => ipcRenderer.invoke('save-file', options)
-});
